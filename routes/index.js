987faa19531e04f744e3933de3b04e45adaf7d17
Rafael Brigati <johndoe@example.comgit config --global user.name Rafael>
2023-07-13 09:25:38 +0200

    Lab cinema

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+const Movie = require('../models/Movie.model');
 
-module.exports = router;
+/* GET home page */
+router.get('/', (req, res, next) => {
+res.render('index')
+});
+
+
+
+router.get('/movies', (req, res) => {
+    Movie.find()
+        .then(allMovies => {
+            res.render('movies', { allMovies });
+        })
+        .catch(err => console.log(err))
+})
+
+router.get('/movie/:movieId', (req, res) => {
+    const { movieId } = req.params
+    Movie
+        .findById(movieId)
+        .then(movie => res.render('movie', movie))
+        .catch(err => console.log(err))
+    })
+
+    module.exports = router;
